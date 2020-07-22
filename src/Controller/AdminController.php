4520620373fe94556a7ca3ade1fba3fe6b96d893
Justine <pignet.justine@gmail.com>
2020-07-22 23:03:14 +0200

    added personal info edit form

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,9 +3,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * @Route("/admin")
@@ -18,5 +22,38 @@ class AdminController extends AbstractController
     public function index(): Response
     {
         return $this->render("admin/index.html.twig");
+    }
+
+    /**
+     * @Route("/edit", name="admin_info")
+     * @param Request $request
+     * @return Response
+     */
+    public function editInfo(
+        Request $request,
+        UserPasswordEncoderInterface $passwordEncoder
+    ): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setEmail($form->get('email')->getData());
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('success', 'your information was successfully updated');
+            return $this->redirectToRoute('admin_index');
+        }
+
+        return $this->render('admin/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
